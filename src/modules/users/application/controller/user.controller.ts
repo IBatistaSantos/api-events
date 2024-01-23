0f691298fde023559138787f27e21ff61e9a1669
Israel Batista <israelbatiista19@gmail.com>
2024-01-23 11:26:26 -0300

    feat: create method in user controller

---
+++ src/modules/users/application/controller/user.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { GetProfileUseCase } from '../useCases/get-profile.usecase';
+import baseRoute from '@/config/routes/base-route';
+import { JwtAuthGuard } from '@/modules/auth/application/guard/jwt-auth.guard';
+import { GetUser } from '@/shared/decorator/get-decorator';
+import { ApplyPermissionUseCase } from '../useCases/apply-permission.usecase';
+import { ApplyPermissionDTO } from './dtos/apply-permission.dto';
+
+@Controller(`${baseRoute.base_url_v1}/users`)
+export class UserController {
+  constructor(
+    private readonly getProfileUseCase: GetProfileUseCase,
+    private readonly applyPermissionsUseCase: ApplyPermissionUseCase,
+  ) {}
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  async getProfile(@GetUser() user: any) {
+    return await this.getProfileUseCase.execute(user.id);
+  }
+
+  @Post('/permissions')
+  @UseGuards(JwtAuthGuard)
+  async applyPermissions(
+    @GetUser() user: any,
+    @Body() params: ApplyPermissionDTO,
+  ) {
+    const data = {
+      adminId: user.id,
+      ...params,
+    };
+    return await this.applyPermissionsUseCase.execute(data);
+  }
+}

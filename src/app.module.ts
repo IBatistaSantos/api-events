51d8a9548c3ad8bab4184c5bfbbaf0c39cbf0bda
Israel Batista <israelbatiista19@gmail.com>
2024-01-23 11:27:54 -0300

    feat: add modules in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './shared/infra/prisma/prisma.module';
 import { AccountModule } from './modules/accounts/account.module';
 import { AuthenticationModule } from './modules/auth/auth.module';
+import { PermissionModule } from './modules/permissions/permission.module';
+import { UserModule } from './modules/users/user.module';
 
 @Module({
-  imports: [PrismaModule, AccountModule, AuthenticationModule],
+  imports: [
+    PrismaModule,
+    AccountModule,
+    AuthenticationModule,
+    PermissionModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })

62627ff0fac178a051e01f2b1da7c279c80a97bb
Israel Batista <israelbatiista19@gmail.com>
2024-01-23 15:23:27 -0300

    feat: tags in controller

--- src/modules/users/application/controller/user.controller.ts
+++ src/modules/users/application/controller/user.controller.ts
@@ -5,8 +5,10 @@ import { JwtAuthGuard } from '@/modules/auth/application/guard/jwt-auth.guard';
 import { GetUser } from '@/shared/decorator/get-decorator';
 import { ApplyPermissionUseCase } from '../useCases/apply-permission.usecase';
 import { ApplyPermissionDTO } from './dtos/apply-permission.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller(`${baseRoute.base_url_v1}/users`)
+@ApiTags('users')
 export class UserController {
   constructor(
     private readonly getProfileUseCase: GetProfileUseCase,

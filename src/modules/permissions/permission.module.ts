89903640ae8f626e070f8787ea16da53696b4848
Israel Batista <israelbatiista19@gmail.com>
2024-01-23 14:40:55 -0300

    feat: add usecase in module

--- src/modules/permissions/permission.module.ts
+++ src/modules/permissions/permission.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { PermissionController } from './application/controller/permission.controller';
 import { CreatePermissionUseCase } from './application/useCases/create-permission.usecase';
 import { PermissionRepositoryPrisma } from './infra/repository/prisma/permission-repository-prisma';
+import { ListPermissionUseCase } from './application/useCases/list-permission.usecase';
 
 @Module({
   controllers: [PermissionController],
   providers: [
     CreatePermissionUseCase,
+    ListPermissionUseCase,
     {
       provide: 'PermissionRepository',
       useClass: PermissionRepositoryPrisma,

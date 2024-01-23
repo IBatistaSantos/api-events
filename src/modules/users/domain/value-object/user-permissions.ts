c44128e4822beab8505ea01a09d53f3b69077676
Israel Batista <israelbatiista19@gmail.com>
2024-01-23 11:20:10 -0300

    feat: create VO user permissions

---
+++ src/modules/users/domain/value-object/user-permissions.ts
@@ -0,0 +1,48 @@
+import { Permission } from '@/modules/permissions/domain/permission';
+
+interface UserPermissionsProps {
+  id: string;
+  name: string;
+  content: string;
+  description?: string;
+}
+
+export class UserPermissions {
+  private _permissions: UserPermissionsProps[];
+
+  constructor(permissions?: Permission[]) {
+    if (!permissions) {
+      this._permissions = [];
+      return;
+    }
+    this._permissions = this.handlePermissions(permissions);
+  }
+
+  get permissions() {
+    return this._permissions;
+  }
+
+  apply(permissions: Permission[]) {
+    this._permissions = this._permissions.concat(
+      this.handlePermissions(permissions),
+    );
+  }
+
+  get value() {
+    return this._permissions.map((permission) => ({
+      id: permission.id,
+      name: permission.name,
+      content: permission.content,
+      description: permission.description,
+    }));
+  }
+
+  private handlePermissions(permissions: Permission[]) {
+    return permissions.map((permission) => ({
+      id: permission.id,
+      name: permission.name,
+      content: permission.content,
+      description: permission.description,
+    }));
+  }
+}

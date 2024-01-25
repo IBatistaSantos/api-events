5e8e53419a19e2ee1350342bdfc64ddbf6902ccd
Israel Batista <israelbatiista19@gmail.com>
2024-01-25 08:53:10 -0300

    feat: add implementation of prisma orm

---
+++ src/modules/organization/infra/repository/prisma/organization-repository-prisma.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@nestjs/common';
+
+import { OrganizationRepository } from '@/modules/organization/application/repository/organization.repository';
+import { Organization } from '@/modules/organization/domain/organization';
+import { Permission } from '@/modules/permissions/domain/permission';
+import { User } from '@/modules/users/domain/user';
+import { PrismaService } from '@/shared/infra/prisma/repository/prisma.client.service';
+
+@Injectable()
+export class OrganizationRepositoryPrisma implements OrganizationRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+  async findByNameAndAccountId(
+    name: string,
+    accountId: string,
+  ): Promise<Organization> {
+    const organization = await this.prismaService.organization.findFirst({
+      where: {
+        name,
+        accountId,
+        status: 'ACTIVE',
+      },
+    });
+
+    if (!organization) return null;
+
+    return new Organization({
+      id: organization.id,
+      name: organization.name,
+      accountId: organization.accountId,
+      createdBy: organization.createdBy,
+      createdAt: organization.createdAt,
+      status: organization.status,
+    });
+  }
+
+  async save(organization: Organization): Promise<void> {
+    await this.prismaService.organization.create({
+      data: {
+        id: organization.id,
+        name: organization.name,
+        accountId: organization.accountId,
+        createdBy: organization.createdBy,
+        description: organization.description,
+        status: organization.status as any,
+        createdAt: organization.createdAt,
+        updatedAt: organization.updatedAt,
+      },
+    });
+  }
+
+  async findByCreator(createdBy: string, accountId: string): Promise<User> {
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        id: createdBy,
+        accountId,
+        status: 'ACTIVE',
+      },
+      include: { permissions: { include: { permission: true } } },
+    });
+
+    if (!user) return null;
+
+    const permissions = user.permissions.map(({ permission }) => {
+      return new Permission({
+        id: permission.id,
+        name: permission.name,
+        description: permission.description,
+        createdAt: permission.createdAt,
+        content: permission.content as any,
+        updatedAt: permission.updatedAt,
+      });
+    });
+
+    return new User({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      type: user.type,
+      accountId: user.accountId,
+      permissions: permissions,
+      status: user.status,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    });
+  }
+}

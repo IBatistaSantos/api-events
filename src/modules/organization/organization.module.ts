b786da6c975b2ca94ccf5351b8b88716948b0e14
Israel Batista <israelbatiista19@gmail.com>
2024-01-26 09:16:13 -0300

    feat: add provoder in organization module

--- src/modules/organization/organization.module.ts
+++ src/modules/organization/organization.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common';
 import { OrganizationController } from './application/controller/organization.controller';
 import { CreateOrganizationUseCase } from './application/useCases/create-organization-usecase';
 import { OrganizationRepositoryPrisma } from './infra/repository/prisma/organization-repository-prisma';
+import { FindOrganizationUseCase } from './application/useCases/find-organization-usecase';
+import { ListOrganizationUseCase } from './application/useCases/list-organization-usecase';
+import { DisableOrganizationUseCase } from './application/useCases/disable-organization-usecase';
+import { UpdateOrganizationUseCase } from './application/useCases/update-organization-usecase';
 
 @Module({
   controllers: [OrganizationController],
   providers: [
     CreateOrganizationUseCase,
+    FindOrganizationUseCase,
+    ListOrganizationUseCase,
+    DisableOrganizationUseCase,
+    UpdateOrganizationUseCase,
     {
       provide: 'OrganizationRepository',
       useClass: OrganizationRepositoryPrisma,

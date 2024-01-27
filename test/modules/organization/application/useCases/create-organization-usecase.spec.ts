9bb223f9e227bf46519ed808d3d6bfe15e6325a1
Israel Batista <israelbatiista19@gmail.com>
2024-01-27 11:24:49 -0300

    test: create unit test for account free in creation organization

--- test/modules/organization/application/useCases/create-organization-usecase.spec.ts
+++ test/modules/organization/application/useCases/create-organization-usecase.spec.ts
@@ -8,10 +8,12 @@ import { ListPermissions } from '@/modules/permissions/domain/list-permisions';
 import { Permission } from '@/modules/permissions/domain/permission';
 import { User } from '@/modules/users/domain/user';
 import { Organization } from '@/modules/organization/domain/organization';
+import { Account } from '@/modules/accounts/domain/account';
 
 describe('CreateOrganizationUseCase', () => {
   let provider: CreateOrganizationUseCase;
   let repository: MockProxy<OrganizationRepository>;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -45,6 +47,14 @@ describe('CreateOrganizationUseCase', () => {
       }),
     );
     repository.save.mockResolvedValue();
+    repository.listByAccountId.mockResolvedValue([]);
+    repository.findByAccountId.mockResolvedValue(
+      new Account({
+        type: 'FREE',
+        accountPermissions: {},
+        id: faker.string.uuid(),
+      }),
+    );
     provider = module.get<CreateOrganizationUseCase>(CreateOrganizationUseCase);
   });
 
@@ -117,5 +127,56 @@ describe('CreateOrganizationUseCase', () => {
         description: faker.lorem.paragraph(),
       }),
     ).rejects.toThrow('User not permitted');
+  });
+
+  it('Deve dar erro caso a conta nao exista', async () => {
+    repository.findByAccountId.mockResolvedValue(undefined);
+
+    await expect(
+      provider.execute({
+        accountId: faker.string.uuid(),
+        createdBy: faker.string.uuid(),
+        name: faker.company.name(),
+        description: faker.lorem.paragraph(),
+      }),
+    ).rejects.toThrow('Account not found');
+  });
+
+  it('Deve dar erro quando o quantidade de organizacao criada exceder o limite para conta gratuito', async () => {
+    repository.listByAccountId.mockResolvedValue([
+      new Organization({
+        accountId: faker.string.uuid(),
+        createdBy: faker.string.uuid(),
+        name: faker.company.name(),
+        id: faker.string.uuid(),
+      }),
+      new Organization({
+        accountId: faker.string.uuid(),
+        createdBy: faker.string.uuid(),
+        name: faker.company.name(),
+        id: faker.string.uuid(),
+      }),
+      new Organization({
+        accountId: faker.string.uuid(),
+        createdBy: faker.string.uuid(),
+        name: faker.company.name(),
+        id: faker.string.uuid(),
+      }),
+      new Organization({
+        accountId: faker.string.uuid(),
+        createdBy: faker.string.uuid(),
+        name: faker.company.name(),
+        id: faker.string.uuid(),
+      }),
+    ]);
+
+    await expect(
+      provider.execute({
+        accountId: faker.string.uuid(),
+        createdBy: faker.string.uuid(),
+        name: faker.company.name(),
+        description: faker.lorem.paragraph(),
+      }),
+    ).rejects.toThrow('Exceeded the maximum number of organizations');
   });
 });

eae20ab8b918ce2f665b57e0def56b897bc6b0bd
Israel Batista <israelbatiista19@gmail.com>
2024-01-27 11:25:38 -0300

    feat: add method in repository

--- src/modules/organization/application/repository/organization.repository.ts
+++ src/modules/organization/application/repository/organization.repository.ts
@@ -1,5 +1,6 @@
 import { User } from '@/modules/users/domain/user';
 import { Organization } from '../../domain/organization';
+import { Account } from '@/modules/accounts/domain/account';
 
 export interface OrganizationRepository {
   findByNameAndAccountId(
@@ -9,5 +10,6 @@ export interface OrganizationRepository {
   save(organization: Organization): Promise<void>;
   findById(id: string): Promise<Organization>;
   findByCreator(createdBy: string, accountId: string): Promise<User>;
+  findByAccountId(accountId: string): Promise<Account>;
   listByAccountId(accountId: string): Promise<Organization[]>;
 }

34fb7e9eff32e37665f602a9a789ed3af8a67898
Israel Batista <israelbatiista19@gmail.com>
2024-01-29 19:05:49 -0300

    feat: add method create adm controller

--- src/modules/users/application/controller/user.controller.ts
+++ src/modules/users/application/controller/user.controller.ts
@@ -5,7 +5,9 @@ import { JwtAuthGuard } from '@/modules/auth/application/guard/jwt-auth.guard';
 import { GetUser } from '@/shared/decorator/get-decorator';
 import { ApplyPermissionUseCase } from '../useCases/apply-permission.usecase';
 import { ApplyPermissionDTO } from './dtos/apply-permission.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateAdministratorUseCase } from '../useCases/create-administrator.usecase';
+import { CreateAdministratorDTO } from './dtos/create-administrator.dto';
 
 @Controller(`${baseRoute.base_url_v1}/users`)
 @ApiTags('users')
@@ -13,16 +15,42 @@ export class UserController {
   constructor(
     private readonly getProfileUseCase: GetProfileUseCase,
     private readonly applyPermissionsUseCase: ApplyPermissionUseCase,
+    private readonly createAdministratorUseCase: CreateAdministratorUseCase,
   ) {}
 
   @Get('me')
   @UseGuards(JwtAuthGuard)
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully created.',
+    schema: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'string',
+          example: 'uuid',
+        },
+        name: {
+          type: 'string',
+          example: 'John Doe',
+        },
+        email: {
+          type: 'string',
+          example: 'anonymous@example',
+        },
+      },
+    },
+  })
   async getProfile(@GetUser() user: any) {
     return await this.getProfileUseCase.execute(user.id);
   }
 
   @Post('/permissions')
   @UseGuards(JwtAuthGuard)
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
   async applyPermissions(
     @GetUser() user: any,
     @Body() params: ApplyPermissionDTO,
@@ -32,5 +60,60 @@ export class UserController {
       ...params,
     };
     return await this.applyPermissionsUseCase.execute(data);
+  }
+
+  @Post('/administrators')
+  @UseGuards(JwtAuthGuard)
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+    schema: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'string',
+          example: 'uuid',
+        },
+        name: {
+          type: 'string',
+          example: 'John Doe',
+        },
+        email: {
+          type: 'string',
+          example: 'admin@example.com',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'number',
+          example: 400,
+        },
+        message: {
+          type: 'string',
+          example: 'Account already exists',
+        },
+        error: {
+          type: 'string',
+          example: 'Bad Request',
+        },
+      },
+    },
+  })
+  async createAdministrators(
+    @GetUser() user: any,
+    @Body() params: CreateAdministratorDTO,
+  ) {
+    return await this.createAdministratorUseCase.execute({
+      email: params.email,
+      name: params.name,
+      organizationIds: params.organizationIds,
+      userId: user.id,
+    });
   }
 }

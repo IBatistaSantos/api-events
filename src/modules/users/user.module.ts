a4f42ddcce029e9a942fb78f428a55b49a7d6a05
Israel Batista <israelbatiista19@gmail.com>
2024-01-29 19:05:12 -0300

    feat: add dependency in providers

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -3,15 +3,27 @@ import { UserController } from './application/controller/user.controller';
 import { GetProfileUseCase } from './application/useCases/get-profile.usecase';
 import { UserRepositoryPrisma } from './infra/repository/prisma.user.repository-prisma';
 import { ApplyPermissionUseCase } from './application/useCases/apply-permission.usecase';
+import { CreateAdministratorUseCase } from './application/useCases/create-administrator.usecase';
+import { MailerSenderProvider } from '@/shared/infra/services/mail/mailerSender/mailer-sender.service';
+import { BcryptProvider } from '@/shared/infra/providers/encrypt/bcrypt/bcrypt-provider';
 
 @Module({
   controllers: [UserController],
   providers: [
     GetProfileUseCase,
     ApplyPermissionUseCase,
+    CreateAdministratorUseCase,
     {
       provide: 'UserRepository',
       useClass: UserRepositoryPrisma,
+    },
+    {
+      provide: 'EmailService',
+      useClass: MailerSenderProvider,
+    },
+    {
+      provide: 'EncryptProvider',
+      useClass: BcryptProvider,
     },
   ],
 })

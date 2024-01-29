e7eb0d5c3aa7d0dc68fc3741cc7bf2f8df5a546e
Israel Batista <israelbatiista19@gmail.com>
2024-01-29 19:04:49 -0300

    chore: add decorator for documentation

--- src/modules/users/application/controller/dtos/apply-permission.dto.ts
+++ src/modules/users/application/controller/dtos/apply-permission.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { ArrayNotEmpty, IsNotEmpty } from 'class-validator';
 
 export class ApplyPermissionDTO {
   @ArrayNotEmpty()
+  @ApiProperty({
+    description: 'Permissions to be applied',
+    example: ['uuid', 'uuid'],
+  })
   permissions: string[];
 
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'User id to apply permissions',
+    example: 'uuid',
+  })
   userId: string;
 }

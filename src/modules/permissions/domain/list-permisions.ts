3406345e8a3c28669723330973f90e3f45a3656c
Israel Batista <israelbatiista19@gmail.com>
2024-01-29 19:03:33 -0300

    feat: add permission for create administrator

--- src/modules/permissions/domain/list-permisions.ts
+++ src/modules/permissions/domain/list-permisions.ts
@@ -1,4 +1,5 @@
 export enum ListPermissions {
+  CREATE_ADMINISTRATOR = 'create-administrator',
   APPLY_PERMISSION = 'apply-permission',
   CREATE_ORGANIZATION = 'create-organization',
   UPDATE_ORGANIZATION = 'update-organization',

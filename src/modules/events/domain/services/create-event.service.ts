48a93d68884b291ee81ca0e201cac48c78cb297b
Israel Batista <israelbatiista19@gmail.com>
2024-01-31 15:24:53 -0300

    test: create event service

---
+++ src/modules/events/domain/services/create-event.service.ts
@@ -0,0 +1,71 @@
+import { DateProvider } from '@/shared/infra/providers/date/date-provider';
+import { Events } from '../events';
+import { Session } from '@/modules/sessions/domain/session';
+
+interface Input {
+  name: string;
+  url: string;
+  date: string[];
+  hourStart: string;
+  hourEnd: string;
+  accountId: string;
+  organizationId: string;
+}
+
+export class CreateEventService {
+  constructor(private readonly dateProvider: DateProvider) {}
+
+  execute(params: Input) {
+    const { accountId, date, hourEnd, hourStart, name, organizationId, url } =
+      params;
+
+    const dateSorted = this.validateDate(date);
+
+    const event = new Events({
+      accountId,
+      name,
+      organizationId,
+      url,
+    });
+
+    const sessions = dateSorted.map((date, index) => {
+      return new Session({
+        eventId: event.id,
+        date,
+        isCurrent: index === 0,
+        hourStart,
+        hourEnd,
+      });
+    });
+
+    return { event, sessions };
+  }
+
+  private validateDate(date: string[]) {
+    if (date.length < 1) {
+      throw new Error('Date is required');
+    }
+
+    const isBeforeDate = date.every((date) => {
+      return this.dateProvider.isBefore(new Date(date), new Date());
+    });
+
+    if (isBeforeDate) {
+      throw new Error('Date is before current date');
+    }
+
+    return this.sortDate(date);
+  }
+
+  private sortDate(date: string[]) {
+    return date.sort((a, b) => {
+      if (new Date(a) < new Date(b)) {
+        return -1;
+      }
+      if (new Date(a) > new Date(b)) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+}

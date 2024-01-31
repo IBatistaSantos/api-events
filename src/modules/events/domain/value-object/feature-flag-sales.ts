e08fe5b993db55990a7cf973db4fdd2faf00c332
Israel Batista <israelbatiista19@gmail.com>
2024-01-30 21:37:14 -0300

    feat: create feature flag in context sales

---
+++ src/modules/events/domain/value-object/feature-flag-sales.ts
@@ -0,0 +1,37 @@
+export interface FeatureFlagsSalesProps {
+  tickets: boolean;
+  hasInstallments: boolean;
+}
+
+export class FeatureFlagsSales {
+  private _tickets: boolean;
+  private _hasInstallments: boolean;
+
+  constructor(props: Partial<FeatureFlagsSalesProps>) {
+    this._tickets = props?.tickets || false;
+    this._hasInstallments = props?.hasInstallments || false;
+
+    this.validate();
+  }
+
+  get tickets(): boolean {
+    return this._tickets;
+  }
+
+  get hasInstallments(): boolean {
+    return this._hasInstallments;
+  }
+
+  get value() {
+    return {
+      tickets: this._tickets,
+      hasInstallments: this._hasInstallments,
+    };
+  }
+
+  private validate() {
+    if (!this._tickets && this._hasInstallments) {
+      throw new Error("You can't have installments without tickets");
+    }
+  }
+}

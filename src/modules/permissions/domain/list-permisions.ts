e71768f4c5377ecd566cc39ef08fb600590f31d5
Israel Batista <israelbatiista19@gmail.com>
2024-01-31 15:25:48 -0300

    feat: add permission create event in list

--- src/modules/permissions/domain/list-permisions.ts
+++ src/modules/permissions/domain/list-permisions.ts
@@ -3,4 +3,5 @@ export enum ListPermissions {
   APPLY_PERMISSION = 'apply-permission',
   CREATE_ORGANIZATION = 'create-organization',
   UPDATE_ORGANIZATION = 'update-organization',
+  CREATE_EVENT = 'create-event',
 }

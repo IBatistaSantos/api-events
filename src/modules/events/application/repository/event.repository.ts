a8ac00cbfc98ff9a1a180eeb50cb59ded2f1a13c
Israel Batista <israelbatiista19@gmail.com>
2024-01-31 15:30:51 -0300

    feat: create interface event repository

---
+++ src/modules/events/application/repository/event.repository.ts
@@ -0,0 +1,15 @@
+import { Session } from '@/modules/sessions/domain/session';
+import { Events } from '../../domain/events';
+import { User } from '@/modules/users/domain/user';
+import { Organization } from '@/modules/organization/domain/organization';
+import { Account } from '@/modules/accounts/domain/account';
+
+export interface EventRepository {
+  save(event: Events): Promise<void>;
+  saveSessions(sessions: Session[]): Promise<void>;
+  countEventsByAccountId(accountId: string): Promise<number>;
+  findByURL(url: string): Promise<Events>;
+  findManagerById(id: string): Promise<User>;
+  findOrganizationById(id: string): Promise<Organization>;
+  findAccountById(id: string): Promise<Account>;
+}

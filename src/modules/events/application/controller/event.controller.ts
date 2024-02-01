2c509718658127632a70dce6f3d43329f41b6c0b
Israel Batista <israelbatiista19@gmail.com>
2024-02-01 18:49:22 -0300

    feat: add method in controller for list events

--- src/modules/events/application/controller/event.controller.ts
+++ src/modules/events/application/controller/event.controller.ts
@@ -1,15 +1,19 @@
 import baseRoute from '@/config/routes/base-route';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { CreateEventUseCase } from '../useCases/create-event-usecase';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from '@/shared/decorator/get-decorator';
 import { CreateEventDTO } from './dtos/create-event.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { ListEventUseCase } from '../useCases/list-event-usecase';
 
 @Controller(`${baseRoute.base_url_v1}/events`)
 @ApiTags('events')
 export class EventController {
-  constructor(private readonly createEventUseCase: CreateEventUseCase) {}
+  constructor(
+    private readonly createEventUseCase: CreateEventUseCase,
+    private readonly listEventUseCase: ListEventUseCase,
+  ) {}
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
@@ -23,6 +27,18 @@ export class EventController {
       ...createEventDto,
       userId,
       accountId,
+    });
+  }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  async listEvents(@GetUser() user: any, @Query() organizationId: string) {
+    const userId = user.id;
+    const accountId = user.accountId;
+    return await this.listEventUseCase.execute({
+      userId,
+      accountId,
+      organizationId,
     });
   }
 }

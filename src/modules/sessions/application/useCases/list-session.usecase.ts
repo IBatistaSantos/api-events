08a71a76c2dcc10cc4a8755a730bc9639aff6aee
Israel Batista <israelbatiista19@gmail.com>
2024-02-02 20:05:53 -0300

    feat: create list session useCase

---
+++ src/modules/sessions/application/useCases/list-session.usecase.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { SessionRepository } from '../repository/session.repository';
+import { Session } from '../../domain/session';
+
+interface Input {
+  eventId: string;
+}
+
+@Injectable()
+export class ListSessionUseCase {
+  constructor(
+    @Inject('SessionRepository')
+    private readonly sessionRepository: SessionRepository,
+  ) {}
+
+  async execute(params: Input) {
+    const { eventId } = params;
+    const sessions = await this.sessionRepository.listByEventId(eventId);
+    return Session.sort(sessions);
+  }
+}

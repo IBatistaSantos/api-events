21cf70bb7613d3a9470ef76f226208f016d3d497
Israel Batista <israelbatiista19@gmail.com>
2024-02-02 19:52:55 -0300

    feat: add method finCurrent

--- src/modules/sessions/application/controller/session.controller.ts
+++ src/modules/sessions/application/controller/session.controller.ts
@@ -1,16 +1,26 @@
 import baseRoute from '@/config/routes/base-route';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { CreateSessionUseCase } from '../useCases/create-session.usecase';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateSessionDTO } from './dtos/create-session.dto';
+import { FindCurrentSessionUseCase } from '../useCases/find-current-session-usecase';
 
 @Controller(`${baseRoute.base_url_v1}/sessions`)
 export class SessionController {
-  constructor(private readonly createSessionUseCase: CreateSessionUseCase) {}
+  constructor(
+    private readonly createSessionUseCase: CreateSessionUseCase,
+    private readonly findCurrentSessionUseCase: FindCurrentSessionUseCase,
+  ) {}
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
   async create(@Body() body: CreateSessionDTO) {
     return await this.createSessionUseCase.execute(body);
+  }
+
+  @Get('event/:eventId/current')
+  @UseGuards(AuthGuard('jwt'))
+  async findCurrent(@Param('eventId') eventId: string) {
+    return await this.findCurrentSessionUseCase.execute({ eventId });
   }
 }

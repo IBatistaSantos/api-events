a8014fa7a9692b88ce79463baebe6bea1bce0154
Israel Batista <israelbatiista19@gmail.com>
2024-02-02 19:40:07 -0300

    feat: create session useCase

---
+++ src/modules/sessions/application/useCases/create-session.usecase.ts
@@ -0,0 +1,79 @@
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { Session } from '../../domain/session';
+import { SessionRepository } from '../repository/session.repository';
+import { DateProvider } from '@/shared/infra/providers/date/date-provider';
+
+interface Input {
+  eventId: string;
+  date: string;
+  hourStart: string;
+  hourEnd: string;
+}
+
+@Injectable()
+export class CreateSessionUseCase {
+  constructor(
+    @Inject('SessionRepository')
+    private readonly sessionRepository: SessionRepository,
+
+    @Inject('DateProvider')
+    private dateProvider: DateProvider,
+  ) {}
+
+  async execute(params: Input) {
+    const { eventId, date, hourEnd, hourStart } = params;
+
+    const isBeforeDate = this.dateProvider.isBefore(new Date(date), new Date());
+
+    if (isBeforeDate) {
+      throw new BadRequestException('Date is before the current date');
+    }
+
+    const session = await this.sessionRepository.findByDate(date, eventId);
+    if (session) {
+      throw new BadRequestException('Session already exists');
+    }
+
+    let isCurrent = false;
+    const sessionCurrent =
+      await this.sessionRepository.findCurrentSession(eventId);
+
+    if (!sessionCurrent) {
+      isCurrent = true;
+    }
+
+    if (sessionCurrent) {
+      const { date: dateCurrent } = sessionCurrent;
+
+      const isBefore = this.dateProvider.isBefore(
+        new Date(date),
+        new Date(dateCurrent),
+      );
+
+      if (isBefore) {
+        sessionCurrent.changeCurrent(false);
+        await this.sessionRepository.update(sessionCurrent);
+        isCurrent = true;
+      }
+    }
+
+    const newSession = new Session({
+      eventId,
+      date,
+      hourStart,
+      hourEnd,
+      isCurrent,
+    });
+
+    await this.sessionRepository.save(newSession);
+
+    return {
+      id: newSession.id,
+      date: newSession.date,
+      hourStart: newSession.hourStart,
+      isCurrent: newSession.isCurrent,
+      eventId: newSession.eventId,
+      hourEnd: newSession.hourEnd,
+    };
+  }
+}

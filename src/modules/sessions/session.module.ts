5840f66faae99e71fc211dbe5a741b1694a6b2f1
Israel Batista <israelbatiista19@gmail.com>
2024-02-02 19:52:45 -0300

    feat: add find current session useCase in provider module

--- src/modules/sessions/session.module.ts
+++ src/modules/sessions/session.module.ts
@@ -3,11 +3,13 @@ import { CreateSessionUseCase } from './application/useCases/create-session.usec
 import { SessionRepositoryPrisma } from './infra/repository/prisma/session-prisma.repository';
 import { DateProviderDateFns } from '@/shared/infra/providers/date/implementations/dateFns/date-provider-datefns';
 import { SessionController } from './application/controller/session.controller';
+import { FindCurrentSessionUseCase } from './application/useCases/find-current-session-usecase';
 
 @Module({
   controllers: [SessionController],
   providers: [
     CreateSessionUseCase,
+    FindCurrentSessionUseCase,
     {
       provide: 'SessionRepository',
       useClass: SessionRepositoryPrisma,

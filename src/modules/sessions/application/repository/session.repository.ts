eb241f869a74fe97a7e64162caea944aa6f7490f
Israel Batista <israelbatiista19@gmail.com>
2024-02-02 19:39:30 -0300

    feat: create implementation for session repository

---
+++ src/modules/sessions/application/repository/session.repository.ts
@@ -0,0 +1,8 @@
+import { Session } from '../../domain/session';
+
+export interface SessionRepository {
+  findByDate(date: string, eventId: string): Promise<Session>;
+  findCurrentSession(eventId: string): Promise<Session>;
+  update(session: Session): Promise<void>;
+  save(session: Session): Promise<void>;
+}
---
+++ src/modules/sessions/infra/repository/prisma/session-prisma.repository.ts
@@ -0,0 +1,77 @@
+import { SessionRepository } from '@/modules/sessions/application/repository/session.repository';
+import { Session } from '@/modules/sessions/domain/session';
+import { PrismaService } from '@/shared/infra/prisma/repository/prisma.client.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SessionRepositoryPrisma implements SessionRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findByDate(date: string, eventId: string): Promise<Session> {
+    const session = await this.prismaService.session.findFirst({
+      where: {
+        date,
+        eventId,
+      },
+    });
+
+    if (!session) return null;
+
+    return new Session({
+      id: session.id,
+      eventId: session.eventId,
+      date: session.date,
+      hourStart: session.hourStart,
+      hourEnd: session.hourEnd,
+      isCurrent: session.isCurrent,
+    });
+  }
+
+  async findCurrentSession(eventId: string): Promise<Session> {
+    const session = await this.prismaService.session.findFirst({
+      where: {
+        eventId,
+        isCurrent: true,
+      },
+    });
+
+    if (!session) return null;
+
+    return new Session({
+      id: session.id,
+      eventId: session.eventId,
+      date: session.date,
+      hourStart: session.hourStart,
+      hourEnd: session.hourEnd,
+      isCurrent: session.isCurrent,
+    });
+  }
+
+  async save(session: Session): Promise<void> {
+    await this.prismaService.session.create({
+      data: {
+        eventId: session.eventId,
+        date: session.date,
+        hourStart: session.hourStart,
+        hourEnd: session.hourEnd,
+        isCurrent: session.isCurrent,
+      },
+    });
+  }
+
+  async update(session: Session): Promise<void> {
+    console.log('update session', session.id);
+    await this.prismaService.session.update({
+      where: {
+        id: session.id,
+      },
+      data: {
+        eventId: session.eventId,
+        date: session.date,
+        hourStart: session.hourStart,
+        hourEnd: session.hourEnd,
+        isCurrent: session.isCurrent,
+      },
+    });
+  }
+}

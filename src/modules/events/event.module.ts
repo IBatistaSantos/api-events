1268a4dc33bc84f4d2e36372bfb84ebc67707b7d
Israel Batista <israelbatiista19@gmail.com>
2024-02-02 09:35:38 -0300

    feat: add list event usecase in list providers

--- src/modules/events/event.module.ts
+++ src/modules/events/event.module.ts
@@ -3,11 +3,13 @@ import { EventController } from './application/controller/event.controller';
 import { CreateEventUseCase } from './application/useCases/create-event-usecase';
 import { DateProviderDateFns } from '@/shared/infra/providers/date/implementations/dateFns/date-provider-datefns';
 import { EventRepositoryPrisma } from './infra/prisma/event-repository-prisma';
+import { ListEventUseCase } from './application/useCases/list-event-usecase';
 
 @Module({
   controllers: [EventController],
   providers: [
     CreateEventUseCase,
+    ListEventUseCase,
     {
       provide: 'EventRepository',
       useClass: EventRepositoryPrisma,

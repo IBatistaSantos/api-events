e155f938d2546a1ea8d83d19e70593021c283ea4
Israel Batista <israelbatiista19@gmail.com>
2024-02-03 09:15:42 -0300

    feat: add method findById

--- src/modules/sessions/application/repository/session.repository.ts
+++ src/modules/sessions/application/repository/session.repository.ts
@@ -1,6 +1,7 @@
 import { Session } from '../../domain/session';
 
 export interface SessionRepository {
+  findById(id: string): Promise<Session>;
   findByDate(date: string, eventId: string): Promise<Session>;
   findCurrentSession(eventId: string): Promise<Session>;
   listByEventId(eventId: string): Promise<Session[]>;

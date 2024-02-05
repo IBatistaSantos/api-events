516d0c8bfce81619950ae4eb1e896817347d5b5b
Israel Batista <israelbatiista19@gmail.com>
2024-02-05 18:43:06 -0300

    feat: create finish session usecase

--- src/modules/sessions/application/useCases/finish-session.usecase.ts
+++ src/modules/sessions/application/useCases/finish-session.usecase.ts
@@ -7,12 +7,14 @@ import {
 import { SessionRepository } from '../repository/session.repository';
 import { Session } from '../../domain/session';
 import { DateProvider } from '@/shared/infra/providers/date/date-provider';
+
 interface Input {
   sessionId: string;
+  hourEnd?: string;
 }
 
 @Injectable()
-export class FindCurrentSessionUseCase {
+export class FinishSessionUseCase {
   constructor(
     @Inject('SessionRepository')
     private readonly sessionRepository: SessionRepository,
@@ -22,7 +24,7 @@ export class FindCurrentSessionUseCase {
   ) {}
 
   async execute(params: Input) {
-    const { sessionId } = params;
+    const { sessionId, hourEnd } = params;
     const session = await this.sessionRepository.findById(sessionId);
 
     if (!session) {
@@ -33,12 +35,15 @@ export class FindCurrentSessionUseCase {
       throw new BadRequestException('Session is not current');
     }
 
-    const isAfterDateSession = this.dateProvider.isAfter(
+    const dateWithHours = `${session.date}T${session.hourStart}`;
+    const isBeforeDateSession = this.dateProvider.isBefore(
       new Date(),
-      new Date(session.date),
+      new Date(dateWithHours),
     );
 
-    if (!isAfterDateSession) {
+    const today = this.dateProvider.today(new Date(dateWithHours));
+
+    if (!isBeforeDateSession && !today) {
       throw new BadRequestException(
         'Session not finished, because the date is not today',
       );
@@ -57,9 +62,7 @@ export class FindCurrentSessionUseCase {
       current.changeCurrent(true);
       await this.sessionRepository.update(current);
     }
-
-    session.finish();
-
+    session.finish(hourEnd);
     await this.sessionRepository.update(session);
   }
 }

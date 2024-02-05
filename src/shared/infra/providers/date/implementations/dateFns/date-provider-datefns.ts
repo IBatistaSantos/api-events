99d0c5d2e85ac2c77659f5f517d7e1a2f2d9a381
Israel Batista <israelbatiista19@gmail.com>
2024-02-05 18:42:28 -0300

    feat: create implementation today method

--- src/shared/infra/providers/date/implementations/dateFns/date-provider-datefns.ts
+++ src/shared/infra/providers/date/implementations/dateFns/date-provider-datefns.ts
@@ -1,7 +1,14 @@
-import { isBefore, isAfter as isAfterFNS } from 'date-fns';
+import {
+  isBefore,
+  isAfter as isAfterFNS,
+  isToday as isTodayFNS,
+} from 'date-fns';
 import { DateProvider } from '../../date-provider';
 
 export class DateProviderDateFns implements DateProvider {
+  today(date: Date): boolean {
+    return isTodayFNS(date);
+  }
   isAfter(date: Date, dateToCompare: Date): boolean {
     return isAfterFNS(dateToCompare, date);
   }

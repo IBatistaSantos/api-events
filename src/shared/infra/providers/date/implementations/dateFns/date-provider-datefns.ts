9bd4d1ee18b849f4f5c5c1a236d0774523a99b6d
Israel Batista <israelbatiista19@gmail.com>
2024-02-04 21:14:20 -0300

    feat: add method in interface date provider

--- src/shared/infra/providers/date/implementations/dateFns/date-provider-datefns.ts
+++ src/shared/infra/providers/date/implementations/dateFns/date-provider-datefns.ts
@@ -1,7 +1,10 @@
-import { isBefore } from 'date-fns';
+import { isBefore, isAfter as isAfterFNS } from 'date-fns';
 import { DateProvider } from '../../date-provider';
 
 export class DateProviderDateFns implements DateProvider {
+  isAfter(date: Date, dateToCompare: Date): boolean {
+    return isAfterFNS(dateToCompare, date);
+  }
   isBefore(date: Date, dateToCompare: Date): boolean {
     return isBefore(date, dateToCompare);
   }

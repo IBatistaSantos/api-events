0b2bb6da4aea8675ade2716a37fee008acdbd22d
Israel Batista <israelbatiista19@gmail.com>
2024-02-06 16:22:52 -0300

    docs: add property in body

--- src/modules/sessions/application/controller/dtos/finish-session.dto.ts
+++ src/modules/sessions/application/controller/dtos/finish-session.dto.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class FinishSessionDTO {
   @IsOptional()
   @IsNotEmpty()
+  @ApiProperty({
+    example: '1',
+    description: 'ID Session',
+    required: false,
+  })
   hourEnd: string;
 }

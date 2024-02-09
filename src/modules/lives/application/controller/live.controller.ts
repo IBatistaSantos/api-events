140aab6bf7f69515fac4aa279a491e98eff5270d
Israel Batista <israelbatiista19@gmail.com>
2024-02-09 17:25:23 -0300

    feat: add method in controller

--- src/modules/lives/application/controller/live.controller.ts
+++ src/modules/lives/application/controller/live.controller.ts
@@ -1,18 +1,34 @@
 import baseRoute from '@/config/routes/base-route';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { CreateLiveUseCase } from '../useCases/create-live.usecase';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateLiveDTO } from './dtos/create-live.dto';
+import { DetailsLiveUseCase } from '../useCases/details-live.usecase';
+import { ListLiveSessionIdUseCase } from '../useCases/list-live-sessionId.usecase';
 
 @Controller(`${baseRoute.base_url_v1}/lives`)
 @ApiTags('Lives')
 export class LiveController {
-  constructor(private readonly createLiveUseCase: CreateLiveUseCase) {}
+  constructor(
+    private readonly createLiveUseCase: CreateLiveUseCase,
+    private readonly detailsLiveUseCase: DetailsLiveUseCase,
+    private readonly listLiveSessionIdUseCase: ListLiveSessionIdUseCase,
+  ) {}
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
   async createLive(@Body() body: CreateLiveDTO) {
     return this.createLiveUseCase.execute(body);
+  }
+
+  @Get('/:liveId')
+  async detailsLive(@Param('liveId') liveId: string) {
+    return this.detailsLiveUseCase.execute(liveId);
+  }
+
+  @Get('/events/:eventId')
+  async listLiveSessionId(@Param('eventId') eventId: string) {
+    return this.listLiveSessionIdUseCase.execute({ eventId });
   }
 }

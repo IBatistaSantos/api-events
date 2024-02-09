9244ea9eeb50ba9f61a9d1b90ee1bd4024a9134f
Israel Batista <israelbatiista19@gmail.com>
2024-02-09 20:02:39 -0300

    feat: create update live usecase

---
+++ src/modules/lives/application/useCases/update-live.usecase.ts
@@ -0,0 +1,63 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { LiveRepository } from '../repository/live.repository';
+
+interface InputData {
+  link: string;
+  typeLink: string;
+  title: string;
+  disableChat: boolean;
+  disableReactions: boolean;
+  isMain: boolean;
+  chat: any;
+  translation: any;
+}
+
+interface Input {
+  liveId: string;
+  data: Partial<InputData>;
+}
+
+@Injectable()
+export class UpdateLiveUseCase {
+  constructor(
+    @Inject('LiveRepository')
+    private readonly repository: LiveRepository,
+  ) {}
+
+  async execute(params: Input) {
+    const { liveId, data } = params;
+
+    const live = await this.repository.findById(liveId);
+    if (!live) {
+      throw new NotFoundException('Live not found');
+    }
+
+    const session = await this.repository.findSessionById(live.sessionId);
+    if (!session) {
+      throw new NotFoundException('Session not found');
+    }
+
+    if (session.finished) {
+      throw new NotFoundException('Session already finished');
+    }
+
+    if (live.finished) {
+      throw new NotFoundException('Live already finished');
+    }
+
+    const { link, typeLink, title, chat, isMain, translation } = data;
+
+    live.update({
+      chat,
+      isMain,
+      title,
+      typeLink,
+      link,
+      translation,
+    });
+
+    await this.repository.update(live);
+
+    return live.toJSON();
+  }
+}

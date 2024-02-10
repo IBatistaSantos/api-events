23bc73b4051ddfb423e66e77dfb660a6710f5a92
Israel Batista <israelbatiista19@gmail.com>
2024-02-10 12:07:17 -0300

    feat: create VO for guest entity

---
+++ src/modules/guests/domain/value-object/guest-status.ts
@@ -0,0 +1,60 @@
+export class GuestStatus {
+  private _value: string;
+
+  private constructor(value: string) {
+    this._value = value;
+  }
+
+  get value(): string {
+    return this._value;
+  }
+
+  get isWaitingApproved(): boolean {
+    return this._value === 'waiting_approved';
+  }
+
+  get isApproved(): boolean {
+    return this._value === 'approved';
+  }
+
+  get isConfirmed(): boolean {
+    return this._value === 'confirmed';
+  }
+
+  get isRefused(): boolean {
+    return this._value === 'refused';
+  }
+
+  private static validate(value: string) {
+    if (
+      !['waiting_approved', 'approved', 'confirmed', 'refused'].includes(value)
+    ) {
+      throw new Error('Invalid status');
+    }
+  }
+
+  public static fromValue(value: string): GuestStatus {
+    if (!value) {
+      return this.waitingApproved();
+    }
+    this.validate(value);
+
+    return new GuestStatus(value);
+  }
+
+  public static waitingApproved(): GuestStatus {
+    return new GuestStatus('waiting_approved');
+  }
+
+  public static approved(): GuestStatus {
+    return new GuestStatus('approved');
+  }
+
+  public static confirmed(): GuestStatus {
+    return new GuestStatus('confirmed');
+  }
+
+  public static refused(): GuestStatus {
+    return new GuestStatus('refused');
+  }
+}

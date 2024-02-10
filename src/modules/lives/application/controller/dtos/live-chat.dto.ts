89670343d68165cca5a9652b0a075d6cd3fc3ed2
Israel Batista <israelbatiista19@gmail.com>
2024-02-10 09:15:07 -0300

    docs: add property the request create live chat in docs

--- src/modules/lives/application/controller/dtos/live-chat.dto.ts
+++ src/modules/lives/application/controller/dtos/live-chat.dto.ts
@@ -1,11 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class LiveChatDTO {
   @IsOptional()
   @IsNotEmpty()
+  @ApiProperty({
+    type: 'string',
+    example: 'Title of the live',
+    required: false,
+  })
   title?: string;
 
   @IsNotEmpty()
   @IsEnum(['open', 'moderate'])
+  @ApiProperty({
+    type: 'string',
+    example: 'open',
+    enum: ['open', 'moderate'],
+    required: false,
+    default: 'open',
+  })
   type?: string;
 }

9714c6ede71f9fcebc7f6027487bb3c301f9b1af
Israel Batista <israelbatiista19@gmail.com>
2024-02-10 09:14:59 -0300

    docs: add property the request create live in docs

--- src/modules/lives/application/controller/dtos/create-live.dto.ts
+++ src/modules/lives/application/controller/dtos/create-live.dto.ts
@@ -7,30 +7,67 @@ import {
 } from 'class-validator';
 import { LiveChatDTO } from './live-chat.dto';
 import { LiveTranslationDTO } from './live-translation.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateLiveDTO {
   @IsNotEmpty()
   @IsUUID()
+  @ApiProperty({
+    type: 'string',
+    format: 'uuid',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    required: true,
+  })
   sessionId: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: 'string',
+    example: 'https://www.youtube.com/watch?v=123456',
+    required: true,
+  })
   link: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: 'string',
+    example: 'YOUTUBE',
+    enum: ['YOUTUBE', 'VIMEO', 'WHEREBY', 'TEAMS', 'ZOOM', 'OTHER'],
+    required: true,
+  })
   typeLink: string;
 
   @IsBoolean()
   @IsOptional()
+  @ApiProperty({
+    type: 'boolean',
+    example: true,
+    required: false,
+  })
   isMain?: boolean;
 
   @IsNotEmpty()
   @IsOptional()
+  @ApiProperty({
+    type: 'string',
+    example: 'Title of the live',
+    required: false,
+  })
   title?: string;
 
   @IsOptional()
+  @ApiProperty({
+    type: LiveChatDTO,
+    required: false,
+  })
   chat?: LiveChatDTO;
 
   @IsOptional()
   @ArrayNotEmpty()
+  @ApiProperty({
+    type: LiveTranslationDTO,
+    isArray: true,
+    required: false,
+  })
   translation?: LiveTranslationDTO[];
 }

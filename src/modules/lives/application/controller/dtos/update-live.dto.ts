51356f5c684a99c1550197b40ccf40e817a20c93
Israel Batista <israelbatiista19@gmail.com>
2024-02-10 09:15:47 -0300

    docs: add property the request update live in docs

--- src/modules/lives/application/controller/dtos/update-live.dto.ts
+++ src/modules/lives/application/controller/dtos/update-live.dto.ts
@@ -6,28 +6,58 @@ import {
 } from 'class-validator';
 import { LiveChatDTO } from './live-chat.dto';
 import { LiveTranslationDTO } from './live-translation.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateLiveDTO {
   @IsNotEmpty()
   @IsOptional()
+  @ApiProperty({
+    type: 'string',
+    example: 'https://www.youtube.com/watch?v=123456',
+    required: false,
+  })
   link: string;
 
   @IsNotEmpty()
   @IsOptional()
+  @ApiProperty({
+    type: 'string',
+    example: 'YOUTUBE',
+    enum: ['YOUTUBE', 'VIMEO', 'WHEREBY', 'TEAMS', 'ZOOM', 'OTHER'],
+    required: false,
+  })
   typeLink: string;
 
   @IsBoolean()
   @IsOptional()
+  @ApiProperty({
+    type: 'boolean',
+    example: true,
+    required: false,
+  })
   isMain?: boolean;
 
   @IsNotEmpty()
   @IsOptional()
+  @ApiProperty({
+    type: 'string',
+    example: 'Title of the live',
+    required: false,
+  })
   title?: string;
 
   @IsOptional()
+  @ApiProperty({
+    type: LiveChatDTO,
+    required: false,
+  })
   chat?: LiveChatDTO;
 
   @IsOptional()
   @ArrayNotEmpty()
+  @ApiProperty({
+    type: LiveTranslationDTO,
+    required: false,
+  })
   translation?: LiveTranslationDTO[];
 }

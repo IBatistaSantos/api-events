ad622549f49fa4242cfa18287031951cfecf0d5b
Israel Batista <israelbatiista19@gmail.com>
2024-02-10 09:15:19 -0300

    docs: add property the request create live translation in docs

--- src/modules/lives/application/controller/dtos/live-translation.dto.ts
+++ src/modules/lives/application/controller/dtos/live-translation.dto.ts
@@ -1,13 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class LiveTranslationDTO {
   @IsNotEmpty()
+  @ApiProperty({
+    type: 'string',
+    example: 'pt',
+    required: true,
+  })
   language: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: 'string',
+    example: 'https://www.youtube.com/watch?v=123456',
+    required: true,
+  })
   link: string;
 
   @IsNotEmpty()
   @IsOptional()
+  @ApiProperty({
+    type: 'string',
+    example: 'Title of the live',
+    required: false,
+  })
   text: string;
 }

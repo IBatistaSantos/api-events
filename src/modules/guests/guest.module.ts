18e78dde0ffd02115cfe6e816ee220e1f35c032b
Israel Batista <israelbatiista19@gmail.com>
2024-02-10 13:18:06 -0300

    feat: add approve guest useCase in module

--- src/modules/guests/guest.module.ts
+++ src/modules/guests/guest.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { GuestController } from './application/controller/guest.controller';
 import { CreateGuestUseCase } from './application/useCases/create-guest.usecase';
 import { GuestPrismaRepository } from './infra/repository/prisma/guest-prisma.repository';
+import { ApproveGuestUseCase } from './application/useCases/approve-guest.usecase';
 
 @Module({
   imports: [],
   controllers: [GuestController],
   providers: [
     CreateGuestUseCase,
+    ApproveGuestUseCase,
     {
       provide: 'GuestRepository',
       useClass: GuestPrismaRepository,

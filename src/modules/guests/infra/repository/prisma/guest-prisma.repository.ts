760e7cc64b5dc2569c0137287e7dab2538464cfd
Israel Batista <israelbatiista19@gmail.com>
2024-02-10 12:43:50 -0300

    feat: create implementation prisma for guest repository

---
+++ src/modules/guests/infra/repository/prisma/guest-prisma.repository.ts
@@ -0,0 +1,58 @@
+import { GuestRepository } from '@/modules/guests/application/repository/guest.repository';
+import { Guest } from '@/modules/guests/domain/guest';
+import { PrismaService } from '@/shared/infra/prisma/repository/prisma.client.service';
+import { Injectable } from '@nestjs/common';
+import { GuestStatus, UserStatus } from '@prisma/client';
+
+@Injectable()
+export class GuestPrismaRepository implements GuestRepository {
+  constructor(private readonly prisma: PrismaService) {}
+  async findByEmail(email: string): Promise<Guest> {
+    const guest = await this.prisma.guest.findFirst({
+      where: {
+        email,
+        status: 'ACTIVE',
+      },
+    });
+
+    if (!guest) {
+      return null;
+    }
+
+    return new Guest({
+      id: guest.id,
+      name: guest.name,
+      email: guest.email,
+      eventId: guest.eventId,
+      isConfirmed: guest.isConfirmed,
+      statusGuest: guest.statusGuest,
+      status: guest.status,
+      approvedAt: guest.approvedAt,
+      approvedBy: guest.approvedBy,
+      rejectedAt: guest.recusedAt,
+      rejectedBy: guest.recusedBy,
+      createdAt: guest.createdAt,
+      updatedAt: guest.updatedAt,
+    });
+  }
+
+  async save(guest: Guest): Promise<void> {
+    await this.prisma.guest.create({
+      data: {
+        id: guest.id,
+        name: guest.name,
+        email: guest.email,
+        eventId: guest.eventId,
+        isConfirmed: guest.isConfirmed,
+        statusGuest: guest.statusGuest as GuestStatus,
+        status: guest.status as UserStatus,
+        approvedAt: guest.approvedAt,
+        approvedBy: guest.approvedBy,
+        recusedAt: guest.rejectedAt,
+        recusedBy: guest.rejectedBy,
+        createdAt: guest.createdAt,
+        updatedAt: guest.updatedAt,
+      },
+    });
+  }
+}

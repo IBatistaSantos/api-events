b28bf35596e2db81dbf073ebbafe827064c6b15d
Israel Batista <israelbatiista19@gmail.com>
2024-02-11 11:00:27 -0300

    feat: create panelist module

---
+++ src/modules/panelist/panelist.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { CreatePanelistUseCase } from './application/useCases/create-panelist.usecase';
+import { PanelistPrismaRepository } from './infra/repository/prisma/panelist-prisma.repository';
+import { PanelistController } from './application/controller/panelist.controller';
+import { DeletePanelistUseCase } from './application/useCases/delete-panelist.usecase';
+import { DetailsPanelistUseCase } from './application/useCases/details-panelist.usecase';
+import { ListPanelistUseCase } from './application/useCases/list-panelist.usecase';
+
+@Module({
+  controllers: [PanelistController],
+  exports: [],
+  imports: [],
+  providers: [
+    CreatePanelistUseCase,
+    DeletePanelistUseCase,
+    DetailsPanelistUseCase,
+    ListPanelistUseCase,
+    {
+      provide: 'PanelistRepository',
+      useClass: PanelistPrismaRepository,
+    },
+  ],
+})
+export class PanelistModule {}

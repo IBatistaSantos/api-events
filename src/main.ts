b129890bbea652ab3b22a74126bdbdd5c46cd742
Israel Batista <israelbatiista19@gmail.com>
2024-02-11 16:57:18 -0300

    feat" add global filters exception handler

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './config/http-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
     .setTitle('Documentation evnts API')
     .setDescription('The evnts API documentation')

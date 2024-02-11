51b934ade2d9c73ead57547b1c4d68d155307bb1
Israel Batista <israelbatiista19@gmail.com>
2024-02-11 17:33:24 -0300

    feat: create send invite guest useCase

---
+++ src/modules/guests/application/useCases/send-request-guest.usecase.ts
@@ -0,0 +1,37 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { GuestRepository } from '../repository/guest.repository';
+import { BadException } from '@/shared/domain/errors/errors';
+import { Guest } from '../../domain/guest';
+
+interface Input {
+  name: string;
+  email: string;
+  eventId: string;
+}
+
+@Injectable()
+export class SendRequestGuestUseCase {
+  constructor(
+    @Inject('GuestRepository')
+    private readonly guestRepository: GuestRepository,
+  ) {}
+
+  async execute(props: Input) {
+    const { name, email, eventId } = props;
+    const existing = await this.guestRepository.findByEmail(email, eventId);
+
+    if (existing) {
+      throw new BadException('Convidado já cadastrado com este email');
+    }
+
+    const guest = new Guest({
+      name,
+      email,
+      eventId,
+    });
+
+    await this.guestRepository.save(guest);
+
+    return guest.toJSON();
+  }
+}

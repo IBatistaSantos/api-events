ef506eff487200f8d9c08eaf32d97dcca3e3b446
Israel Batista <israelbatiista19@gmail.com>
2024-02-12 11:12:25 -0300

    feat: add method in contract the panelist repository

--- src/modules/panelist/application/repository/panelist.repository.ts
+++ src/modules/panelist/application/repository/panelist.repository.ts
@@ -4,6 +4,7 @@ export interface PanelistRepository {
   findByEmail(email: string, eventId: string): Promise<Panelist | undefined>;
   findById(id: string): Promise<Panelist | undefined>;
   findByEventId(eventId: string): Promise<Panelist[]>;
+  countByEventId(eventId: string): Promise<number>;
   save(panelist: Panelist): Promise<void>;
   update(panelist: Panelist): Promise<void>;
 }

2c755992dc95bc1d31c981ccd8b5621542576e13
Israel Batista <israelbatiista19@gmail.com>
2024-02-12 11:46:46 -0300

    feat: add method of updateMany and findByIds in contracts panelist repository

--- src/modules/panelist/application/repository/panelist.repository.ts
+++ src/modules/panelist/application/repository/panelist.repository.ts
@@ -3,8 +3,10 @@ import { Panelist } from '../../domain/panelist';
 export interface PanelistRepository {
   findByEmail(email: string, eventId: string): Promise<Panelist | undefined>;
   findById(id: string): Promise<Panelist | undefined>;
+  findByIds(ids: string[]): Promise<Panelist[]>;
   findByEventId(eventId: string): Promise<Panelist[]>;
   countByEventId(eventId: string): Promise<number>;
   save(panelist: Panelist): Promise<void>;
   update(panelist: Panelist): Promise<void>;
+  updateMany(panelists: Panelist[]): Promise<void>;
 }

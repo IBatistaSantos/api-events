cffbefaa0cdcdf1d1f918a9f37b57b0b2b0c78a9
Israel Batista <israelbatiista19@gmail.com>
2024-02-16 11:57:27 -0300

    feat: add validation permissions in list guest useCase

--- src/modules/guests/application/useCases/list-guests.usecase.ts
+++ src/modules/guests/application/useCases/list-guests.usecase.ts
@@ -2,9 +2,12 @@ import { Inject, Injectable } from '@nestjs/common';
 
 import { GuestRepository } from '../repository/guest.repository';
 import { Guest } from '../../domain/guest';
+import { BadException, NotFoundException } from '@/shared/domain/errors/errors';
+import { ListPermissions } from '@/modules/permissions/domain/list-permisions';
 
 interface Input {
   eventId: string;
+  userId: string;
 }
 
 @Injectable()
@@ -15,7 +18,32 @@ export class ListGuestUseCase {
   ) {}
 
   async execute(params: Input) {
-    const { eventId } = params;
+    const { eventId, userId } = params;
+
+    const event = await this.guestRepository.findEvent(eventId);
+    if (!event) {
+      throw new NotFoundException('Evento não encontrado');
+    }
+
+    const manager = await this.guestRepository.findApprovedGuest(userId);
+    if (!manager) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    const isSameAccount = manager.accountId === event.accountId;
+    if (!isSameAccount) {
+      throw new BadException(
+        'Usuário não tem permissão para aprovar convidados',
+      );
+    }
+
+    const isCanAction = manager.can(ListPermissions.MANAGER_EVENT);
+    if (!isCanAction) {
+      throw new BadException(
+        'Usuário não tem permissão para aprovar convidados',
+      );
+    }
+
     const listGuests = await this.guestRepository.listByEventId(eventId);
 
     return Guest.sort(listGuests).map((guest) => guest.toJSON());

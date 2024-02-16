17bc7e46570b57680da28b2a06dc800f3c518cb4
Israel Batista <israelbatiista19@gmail.com>
2024-02-16 11:56:22 -0300

    feat: create unit for permissions validation

--- test/modules/guests/application/useCases/approve-guest.usecase.spec.ts
+++ test/modules/guests/application/useCases/approve-guest.usecase.spec.ts
@@ -5,10 +5,13 @@ import { MockProxy, mock } from 'jest-mock-extended';
 import { GuestRepository } from '@/modules/guests/application/repository/guest.repository';
 import { Guest } from '@/modules/guests/domain/guest';
 import { ApproveGuestUseCase } from '@/modules/guests/application/useCases/approve-guest.usecase';
+import { Events } from '@/modules/events/domain/events';
+import { User } from '@/modules/users/domain/user';
 
 describe('ApproveGuestUseCase', () => {
   let provider: ApproveGuestUseCase;
   let repository: MockProxy<GuestRepository>;
+  let accountId: string;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,6 +34,30 @@ describe('ApproveGuestUseCase', () => {
         name: faker.person.fullName(),
         isConfirmed: false,
         statusGuest: 'waiting_approved',
+        status: 'ACTIVE',
+      }),
+    );
+
+    accountId = faker.string.uuid();
+    repository.findEvent.mockResolvedValue(
+      new Events({
+        accountId,
+        name: faker.word.sample(),
+        organizationId: faker.string.uuid(),
+        private: true,
+        url: faker.internet.url(),
+        type: 'DIGITAL',
+      }),
+    );
+
+    repository.findApprovedGuest.mockResolvedValue(
+      new User({
+        accountId,
+        email: faker.internet.email(),
+        id: faker.string.uuid(),
+        name: faker.person.fullName(),
+        type: 'MASTER',
+        password: faker.internet.password(),
         status: 'ACTIVE',
       }),
     );
@@ -62,6 +89,101 @@ describe('ApproveGuestUseCase', () => {
         guestId: faker.string.uuid(),
         approvedBy: faker.string.uuid(),
       }),
-    ).rejects.toThrow('Guest not found');
+    ).rejects.toThrow('Convidado não encontrado');
+  });
+
+  it('Deve retornar erro se o evento não existir', async () => {
+    repository.findEvent.mockResolvedValueOnce(undefined);
+
+    await expect(
+      provider.execute({
+        guestId: faker.string.uuid(),
+        approvedBy: faker.string.uuid(),
+      }),
+    ).rejects.toThrow('Evento não encontrado');
+  });
+
+  it('Deve retornar erro se o evento não for privado', async () => {
+    repository.findEvent.mockResolvedValueOnce(
+      new Events({
+        accountId: faker.string.uuid(),
+        name: faker.word.sample(),
+        organizationId: faker.string.uuid(),
+        private: false,
+        url: faker.internet.url(),
+        type: 'DIGITAL',
+      }),
+    );
+
+    await expect(
+      provider.execute({
+        guestId: faker.string.uuid(),
+        approvedBy: faker.string.uuid(),
+      }),
+    ).rejects.toThrow('O evento não é privado');
+  });
+
+  it('Deve retornar erro se o usuário não existir', async () => {
+    repository.findApprovedGuest.mockResolvedValueOnce(undefined);
+
+    await expect(
+      provider.execute({
+        guestId: faker.string.uuid(),
+        approvedBy: faker.string.uuid(),
+      }),
+    ).rejects.toThrow('Usuário não encontrado');
+  });
+
+  it('Deve retornar erro se o usuário não tiver permissão para aprovar convidados', async () => {
+    repository.findApprovedGuest.mockResolvedValueOnce(
+      new User({
+        accountId,
+        email: faker.internet.email(),
+        id: faker.string.uuid(),
+        name: faker.person.fullName(),
+        type: 'ADMIN',
+        password: faker.internet.password(),
+        status: 'ACTIVE',
+      }),
+    );
+
+    await expect(
+      provider.execute({
+        guestId: faker.string.uuid(),
+        approvedBy: faker.string.uuid(),
+      }),
+    ).rejects.toThrow('Usuário não tem permissão para aprovar convidados');
+  });
+
+  it('Deve retornar erro se o usuário for a mesma conta', async () => {
+    repository.findApprovedGuest.mockResolvedValueOnce(
+      new User({
+        accountId: faker.string.uuid(),
+        email: faker.internet.email(),
+        id: faker.string.uuid(),
+        name: faker.person.fullName(),
+        type: 'MASTER',
+        password: faker.internet.password(),
+        status: 'ACTIVE',
+      }),
+    );
+
+    repository.findEvent.mockResolvedValueOnce(
+      new Events({
+        accountId: faker.string.uuid(),
+        name: faker.word.sample(),
+        organizationId: faker.string.uuid(),
+        private: true,
+        url: faker.internet.url(),
+        type: 'DIGITAL',
+      }),
+    );
+
+    await expect(
+      provider.execute({
+        guestId: faker.string.uuid(),
+        approvedBy: faker.string.uuid(),
+      }),
+    ).rejects.toThrow('Usuário não tem permissão para aprovar convidados');
   });
 });

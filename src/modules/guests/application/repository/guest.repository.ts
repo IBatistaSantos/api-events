5364fa9cfa64f40e214bb33fc0f25aaf2762a401
Israel Batista <israelbatiista19@gmail.com>
2024-02-16 11:55:03 -0300

    feat: add method in contracts guest repository

--- src/modules/guests/application/repository/guest.repository.ts
+++ src/modules/guests/application/repository/guest.repository.ts
@@ -1,9 +1,13 @@
+import { Events } from '@/modules/events/domain/events';
 import { Guest } from '@/modules/guests/domain/guest';
+import { User } from '@/modules/users/domain/user';
 
 export interface GuestRepository {
   findById(id: string): Promise<Guest | null>;
   findByEmail(email: string, eventId: string): Promise<Guest | null>;
   findByEmails(emails: string[], eventId: string): Promise<Guest[]>;
+  findEvent(eventId: string): Promise<Events>;
+  findApprovedGuest(email: string): Promise<User>;
   listByEventId(eventId: string): Promise<Guest[]>;
   save(guest: Guest): Promise<void>;
 }

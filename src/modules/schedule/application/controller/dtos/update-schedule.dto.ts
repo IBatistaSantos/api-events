7bbb91f0e4cf7d5adae92207d0a6a2b2fddfe9f1
Israel Batista <israelbatiista19@gmail.com>
2024-02-17 17:59:46 -0300

    feat: update schedule DTO

---
+++ src/modules/schedule/application/controller/dtos/update-schedule.dto.ts
@@ -0,0 +1,68 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsOptional, IsString, IsUUID } from 'class-validator';
+
+export class UpdateScheduleDTO {
+  @IsUUID('4', {
+    message: 'O id da sessão deve ser um UUID válido',
+  })
+  @ApiProperty({
+    type: 'string',
+    format: 'uuid',
+  })
+  @IsOptional()
+  sessionId: string;
+
+  @IsString({
+    message: 'O título deve ser uma string válida',
+  })
+  @ApiProperty({
+    type: 'string',
+    example: 'Título do item da agenda',
+  })
+  @IsOptional()
+  title: string;
+
+  @IsString({
+    message: 'A descrição deve ser uma string válida',
+  })
+  @IsOptional()
+  @ApiProperty({
+    type: 'string',
+    required: false,
+    example: 'Descrição do item da agenda',
+  })
+  description?: string;
+
+  @IsEnum(['schedule', 'warning', 'break'], {
+    message: 'O tipo deve ser um dos valores: schedule, warning, break',
+  })
+  @ApiProperty({
+    type: 'string',
+    enum: ['schedule', 'warning', 'break'],
+    example: 'schedule',
+  })
+  @IsOptional()
+  type?: string;
+
+  @IsString({
+    message: 'A hora de início deve ser uma string válida',
+  })
+  @IsOptional()
+  @ApiProperty({
+    type: 'string',
+    required: false,
+    example: '09:00',
+  })
+  hourStart?: string;
+
+  @IsString({
+    message: 'A hora de fim deve ser uma string válida',
+  })
+  @IsOptional()
+  @ApiProperty({
+    type: 'string',
+    required: false,
+    example: '09:00',
+  })
+  hourEnd?: string;
+}

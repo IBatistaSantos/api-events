7f6c1d8b11ffc3cf807272e216ed9588b313d7a0
Israel Batista <israelbatiista19@gmail.com>
2024-02-19 15:44:46 -0300

    test: create unit test for voting

--- test/modules/votings/domain/voting.spec.ts
+++ test/modules/votings/domain/voting.spec.ts
@@ -3,19 +3,91 @@ import { Voting } from '@/modules/votings/domain/voting';
 describe('Voting', () => {
   it('Deve criar uma enquete', () => {
     const voting = new Voting({
-      questions: 'Qual é o seu nome?',
+      questions: [
+        {
+          title: 'Qual é o seu nome?',
+          type: 'text',
+        },
+      ],
     });
 
-    expect(voting.questions).toBe('Qual é o seu nome?');
+    expect(voting.questions.length).toBe(1);
+    expect(voting.questions).toEqual([
+      {
+        id: expect.any(String),
+        description: null,
+        options: null,
+        title: 'Qual é o seu nome?',
+        type: 'text',
+      },
+    ]);
     expect(voting.id).toBeDefined();
     expect(voting.activated).toBeFalsy();
     expect(voting.targetAudience).toBe('all');
+    expect(voting.createdAt).toBeDefined();
+    expect(voting.updatedAt).toBeDefined();
+    expect(voting.startDate).toBeUndefined();
+    expect(voting.endDate).toBeUndefined();
+    expect(voting.timeInSeconds).toBeNull();
+    expect(voting.status).toBeDefined();
+  });
+
+  it('Deve ativar a enquete', () => {
+    const voting = new Voting({
+      questions: [
+        {
+          title: 'Qual é o seu nome?',
+          type: 'text',
+        },
+      ],
+    });
+
+    voting.activate();
+
+    expect(voting.activated).toBeTruthy();
+    expect(voting.startDate).toBeDefined();
+  });
+
+  it('Deve desativar a enquete', () => {
+    const voting = new Voting({
+      questions: [
+        {
+          title: 'Qual é o seu nome?',
+          type: 'text',
+        },
+      ],
+    });
+
+    voting.activate();
+    voting.deactivate();
+
+    expect(voting.activated).toBeFalsy();
+  });
+
+  it('Deve lançar uma exceção se a enquete ainda nao tenha sido ativada', () => {
+    const voting = new Voting({
+      questions: [
+        {
+          title: 'Qual é o seu nome?',
+          type: 'text',
+        },
+      ],
+    });
+
+    expect(() => {
+      voting.deactivate();
+    }).toThrow('A enquete ainda não foi ativada');
   });
 
   it('Deve lançar uma exceção se o publico algo for invalido', () => {
     expect(() => {
       new Voting({
-        questions: 'Qual é o seu nome?',
+        questions: [
+          {
+            title: 'Qual é o seu nome?',
+            type: 'text',
+          },
+        ],
         targetAudience: 'invalid' as any,
       });
     }).toThrow('Publico alvo inválido');

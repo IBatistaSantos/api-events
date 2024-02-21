689eea93ea6daf924e82aaf0d4de47d3f6af2624
Israel Batista <israelbatiista19@gmail.com>
2024-02-21 20:04:09 -0300

    feat: create field text

---
+++ src/modules/form/domain/fields/field-text.ts
@@ -0,0 +1,39 @@
+import { BadException } from '@/shared/domain/errors/errors';
+import { Field, FieldProps } from '../field';
+
+interface FieldTextProps extends FieldProps {
+  regexValidation?: string;
+}
+
+export class FieldText extends Field {
+  private _regexValidation: string;
+  constructor(props: FieldTextProps) {
+    super({ ...props, type: 'text' });
+
+    this._regexValidation = props.regexValidation || null;
+  }
+
+  get regexValidation(): string {
+    return this._regexValidation;
+  }
+
+  validateField(value: any): void {
+    if (this._required && !value) {
+      throw new BadException('Texto é obrigatório');
+    }
+
+    if (
+      this._regexValidation &&
+      !new RegExp(this._regexValidation).test(value)
+    ) {
+      throw new BadException('Texto inválido');
+    }
+  }
+
+  toJSON() {
+    return {
+      ...super.toJSON(),
+      regexValidation: this._regexValidation,
+    };
+  }
+}

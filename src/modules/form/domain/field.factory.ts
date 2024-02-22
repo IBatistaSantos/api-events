11ce397b2249f6b87ce9b1a30c5e545016ff1e44
Israel Batista <israelbatiista19@gmail.com>
2024-02-22 19:58:34 -0300

    feat: reorder imports

--- src/modules/form/domain/field.factory.ts
+++ src/modules/form/domain/field.factory.ts
@@ -1,18 +1,20 @@
 import { BadException } from '@/shared/domain/errors/errors';
+
 import { Field, FieldProps } from './field';
-import { FieldCheckbox, FieldCheckboxProps } from './fields/field-checkbok';
+import { FieldSelectableProps } from './field-selectable';
+
 import { FieldNumber } from './fields/field-number';
-import { FieldSelect, FieldSelectProps } from './fields/field-select';
+import { FieldSelect } from './fields/field-select';
 import { FieldText } from './fields/field-text';
 import { FieldPassword } from './fields/field-password';
-
+import { FieldCheckbox, FieldCheckboxProps } from './fields/field-checkbok';
 export class FieldFactory {
   static create(field: FieldProps): Field {
     switch (field.type) {
       case 'text':
         return new FieldText(field);
       case 'select':
-        return new FieldSelect(field as FieldSelectProps);
+        return new FieldSelect(field as FieldSelectableProps);
       case 'checkbox':
         return new FieldCheckbox(field as FieldCheckboxProps);
       case 'number':

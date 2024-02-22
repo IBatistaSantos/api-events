7e64a59c1beb7266cebbf0abd7ab6c26d3df18ff
Israel Batista <israelbatiista19@gmail.com>
2024-02-22 19:59:03 -0300

    feat: build options in checkbox

--- src/modules/form/domain/fields/field-checkbok.ts
+++ src/modules/form/domain/fields/field-checkbok.ts
@@ -1,6 +1,7 @@
 import { BadException } from '@/shared/domain/errors/errors';
 import { Field, FieldProps } from '../field';
-import { Option } from '../form';
+import { Option, OptionProps } from '../form';
+import { FieldFactory } from '../field.factory';
 
 export interface FieldCheckboxProps extends FieldProps {
   options: Option[];
@@ -11,18 +12,60 @@ export class FieldCheckbox extends Field {
   constructor(props: FieldCheckboxProps) {
     super({ ...props, type: 'checkbox' });
 
-    this._options = props.options;
+    this._options = this.buildOptions(props.options);
   }
 
   get options() {
     return this._options;
   }
 
+  private buildOptions(options: OptionProps[]): Option[] {
+    if (!options.length) {
+      throw new BadException('Opções são obrigatórias');
+    }
+
+    if (options.length < 2) {
+      throw new BadException('Deve haver pelo menos duas opções');
+    }
+
+    return options.map((option) => {
+      const { label, value, additionalFields } = option;
+
+      if (!label || !value) {
+        throw new BadException('Label e value são obrigatórios');
+      }
+
+      if (additionalFields) {
+        additionalFields.forEach((field) => {
+          if (!field.label || !field.type) {
+            throw new BadException('Label e type são obrigatórios');
+          }
+
+          if (field.options) {
+            field.options.forEach((option) => {
+              if (!option.label || !option.value) {
+                throw new BadException('Label e value são obrigatórios');
+              }
+            });
+          }
+        });
+
+        const fields = additionalFields.map((field) =>
+          FieldFactory.create(field),
+        );
+        return { label, value, additionalFields: fields };
+      }
+
+      return { label, value };
+    });
+  }
+
   validateField(info: Record<string, any>): void {
     const value = info[this.label];
-
     if (this._required && !value) {
-      throw new BadException(`O campo ${this.label} é obrigatório`);
+      throw new BadException(
+        `O campo ${this.label} é obrigatório. Selecione uma opção válida`,
+      );
     }
 
     const isOptionValid = this._options.some(
@@ -32,6 +75,23 @@ export class FieldCheckbox extends Field {
       throw new BadException(
         `O campo ${this.label} é inválido. Selecione uma opção válida`,
       );
+    }
+
+    this.validateAdditionalFields(info);
+  }
+
+  validateAdditionalFields(info: Record<string, any>): void {
+    const value = info[this._label];
+
+    const option = this._options.find((option) => option.value === value);
+    if (!option) {
+      return;
+    }
+
+    if (option.additionalFields) {
+      for (const field of option.additionalFields) {
+        field.validateField(info);
+      }
     }
   }
 

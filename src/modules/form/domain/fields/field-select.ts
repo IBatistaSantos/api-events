a20a4002bbeff8d880c4622eed09ff56fa53ec91
Israel Batista <israelbatiista19@gmail.com>
2024-02-22 19:57:58 -0300

    feat: add extends selectable field

--- src/modules/form/domain/fields/field-select.ts
+++ src/modules/form/domain/fields/field-select.ts
@@ -1,54 +1,7 @@
-import { BadException } from '@/shared/domain/errors/errors';
-import { Field, FieldProps } from '../field';
-import { Option } from '../form';
+import { FieldSelectable, FieldSelectableProps } from '../field-selectable';
 
-export interface FieldSelectProps extends FieldProps {
-  options: Option[];
-}
-
-export class FieldSelect extends Field {
-  private _options: Option[];
-
-  constructor(props: FieldSelectProps) {
-    super({ ...props, type: 'select' });
-
-    this._options = props.options;
-
-    if (!this._options.length) {
-      throw new BadException('Opções são obrigatórias');
-    }
-
-    if (this._options.length < 2) {
-      throw new BadException('Deve haver pelo menos duas opções');
-    }
-  }
-
-  validateField(info: Record<string, any>): void {
-    const value = info[this.label];
-    if (this._required && !value) {
-      throw new BadException(
-        `O campo ${this.label} é obrigatório. Selecione uma opção válida`,
-      );
-    }
-
-    const isOptionValid = this._options.some(
-      (option) => option.value === value,
-    );
-    if (!isOptionValid) {
-      throw new BadException(
-        `O campo ${this.label} é inválido. Selecione uma opção válida`,
-      );
-    }
-  }
-
-  get options() {
-    return this._options;
-  }
-
-  toJSON() {
-    return {
-      ...super.toJSON(),
-      options: this._options,
-    };
+export class FieldSelect extends FieldSelectable {
+  constructor(props: FieldSelectableProps) {
+    super({ ...props, type: 'select', options: props.options });
   }
 }

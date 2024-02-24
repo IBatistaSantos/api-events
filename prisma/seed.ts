c21705de8868d20ae4fd674567d3dafd1c5cbe59
Israel Batista <israelbatiista19@gmail.com>
2024-02-24 13:09:41 -0300

    chore: create template default seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -112,6 +112,33 @@ async function seed() {
 
     console.log(`Permission ${name} created`);
   }
+
+  const templates = [
+    {
+      body: "<div style='width: 100%;max-width: 600px;margin: 0 auto;padding: 20px;text-align:center;'><div style='margin-bottom: 20px;'><img style='max-width: 200px;height: auto;' src='https://evnts.com.br/wp-content/uploads/2021/08/logo-evnts-1-1.png' alt='Logo da Evnts'></div><div style=' background-color: #fff;padding: 20px;border-radius: 10px;box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);'><h2>Recuperação de Senha</h2><p style= 'margin-bottom: 20px;'>Olá {{name}},</p><p style= 'margin-bottom: 20px;'>Você solicitou a recuperação de senha. Clique no botão abaixo para redefinir sua senha.</p><a href='{{link}}'style=' display: inline-block;background-color: #550098;color: #fff;text-decoration: none;padding: 10px 20px;border-radius: 5px;'>Redefinir Senha</a></div></div>",
+      content: 'forgot_password',
+      subject: 'Recuperação de senha',
+    },
+  ];
+
+  for (const template of templates) {
+    const { body, content, subject } = template;
+    const templateExists = await prisma.template.findFirst({
+      where: { content },
+    });
+
+    if (!templateExists) {
+      await prisma.template.create({
+        data: {
+          body,
+          content,
+          subject,
+        },
+      });
+    }
+
+    console.log(`Template ${content} created`);
+  }
 }
 
 seed()

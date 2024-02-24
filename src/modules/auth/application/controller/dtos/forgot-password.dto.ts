8db310a551cc1ab688e3fabdd89ed93cf3014a74
Israel Batista <israelbatiista19@gmail.com>
2024-02-23 22:47:10 -0300

    feat: create DTO for forgot/reset password

--- src/modules/auth/application/controller/dtos/forgot-password.dto.ts
+++ src/modules/auth/application/controller/dtos/forgot-password.dto.ts
@@ -1,7 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class ForgotPasswordDTO {
   @IsNotEmpty({ message: 'O email é obrigatório' })
   @IsEmail({}, { message: 'Email inválido' })
+  @ApiProperty({
+    example: 'any@email.com',
+  })
   email: string;
 }
--- src/modules/auth/application/controller/dtos/reset-password.dto.ts
+++ src/modules/auth/application/controller/dtos/reset-password.dto.ts
@@ -1,15 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, ValidateIf } from 'class-validator';
 
 export class ResetPasswordDTO {
   @IsNotEmpty({ message: 'Senha é obrigatória' })
+  @ApiProperty({
+    example: 'any-password',
+  })
   password: string;
 
   @IsNotEmpty({ message: 'Confirmação de senha é obrigatória' })
   @ValidateIf((object, value) => value !== object.password, {
     message: 'As senhas não conferem',
   })
+  @ApiProperty({
+    example: 'any-password',
+  })
   confirmPassword: string;
 
   @IsNotEmpty({ message: 'Token é obrigatório' })
+  @ApiProperty({
+    example: 'any-token',
+  })
   token: string;
 }

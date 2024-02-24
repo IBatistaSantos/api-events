a82385bb5ecd481a3c3a402caed32b802306179a
Israel Batista <israelbatiista19@gmail.com>
2024-02-24 16:15:32 -0300

    feat: create sender mail useCase

---
+++ src/modules/notifications/application/useCases/send-mail.usecase.ts
@@ -0,0 +1,52 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { NotificationRepository } from '../repository/notification.repository';
+import { TemplateContext } from '../../domain/template';
+import { MailProvider } from '../providers/mail.provider';
+
+interface SendEmailParams {
+  context: TemplateContext;
+  to: {
+    name: string;
+    email: string;
+  };
+  organizationId?: string;
+  eventId?: string;
+  variables?: Record<string, unknown>;
+}
+
+@Injectable()
+export class SendMailUseCase {
+  constructor(
+    @Inject('MailProvider')
+    private readonly provider: MailProvider,
+
+    @Inject('NotificationRepository')
+    private readonly repository: NotificationRepository,
+  ) {}
+
+  async execute(params: SendEmailParams): Promise<void> {
+    const { context, to, variables } = params;
+
+    const template = await this.repository.findByContext(context);
+    if (!template) {
+      throw new NotFoundException('Template nao encontrado');
+    }
+
+    const { body, subject } = template.parse(variables);
+
+    const messageId = await this.provider.send({
+      to,
+      subject,
+      body,
+    });
+
+    console.log(`Email sent with messageId: ${messageId}`);
+
+    await this.repository.save({
+      messageId,
+      templateId: template.id,
+      to: to.email,
+      context,
+    });
+  }
+}

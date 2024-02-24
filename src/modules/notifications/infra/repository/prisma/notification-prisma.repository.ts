7a0dee87c3ebfe00c0008679719519420c377c99
Israel Batista <israelbatiista19@gmail.com>
2024-02-24 13:14:36 -0300

    feat: create implementation notification repository with prisma ORM

---
+++ src/modules/notifications/infra/repository/prisma/notification-prisma.repository.ts
@@ -0,0 +1,48 @@
+import {
+  NotificationRepository,
+  SaveSenderEmailParams,
+} from '@/modules/notifications/application/repository/notification.repository';
+import {
+  TemplateContext,
+  Template,
+} from '@/modules/notifications/domain/template';
+import { PrismaService } from '@/shared/infra/prisma/repository/prisma.client.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class NotificationPrismaRepository implements NotificationRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+  async findByContext(context: TemplateContext): Promise<Template> {
+    const template = await this.prismaService.template.findFirst({
+      where: {
+        content: context,
+        status: 'ACTIVE',
+      },
+    });
+
+    if (!template) {
+      return null;
+    }
+
+    return new Template({
+      body: template.body,
+      context: template.content as TemplateContext,
+      subject: template.subject,
+      createdAt: template.createdAt,
+      id: template.id,
+      status: template.status,
+      updatedAt: template.updatedAt,
+    });
+  }
+
+  async save(params: SaveSenderEmailParams): Promise<void> {
+    await this.prismaService.sendMail.create({
+      data: {
+        messageId: params.messageId,
+        content: params.context,
+        to: params.to,
+        templateId: params.templateId,
+      },
+    });
+  }
+}

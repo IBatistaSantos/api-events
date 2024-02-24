912127e725aa00ac0de3e0dd5342317d44b82ab0
Israel Batista <israelbatiista19@gmail.com>
2024-02-24 16:37:17 -0300

    feat: add type for method list events

--- src/modules/events/application/repository/event.repository.ts
+++ src/modules/events/application/repository/event.repository.ts
@@ -4,6 +4,29 @@ import { User } from '@/modules/users/domain/user';
 import { Organization } from '@/modules/organization/domain/organization';
 import { Account } from '@/modules/accounts/domain/account';
 
+interface SessionOutput {
+  date: string;
+  hourEnd: string;
+  hourStart: string;
+  isCurrent: boolean;
+  id: string;
+}
+
+export interface ListEventOutput {
+  accountId: string;
+  name: string;
+  organizationId: string;
+  url: string;
+  id: string;
+  createdAt: Date;
+  private: boolean;
+  inscriptionType: string;
+  status: string;
+  sessions: SessionOutput[];
+  type: string;
+  updatedAt: Date;
+}
+
 export interface EventRepository {
   save(event: Events): Promise<void>;
   saveSessions(sessions: Session[]): Promise<void>;
@@ -12,5 +35,5 @@ export interface EventRepository {
   findManagerById(id: string): Promise<User>;
   findOrganizationById(id: string): Promise<Organization>;
   findAccountById(id: string): Promise<Account>;
-  list(accountId: string, organizationId?: string): Promise<Events[]>;
+  list(accountId: string, organizationId?: string): Promise<ListEventOutput[]>;
 }

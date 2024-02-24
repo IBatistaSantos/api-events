e038334cf879999737eb01ee316766ebb19439d9
Israel Batista <israelbatiista19@gmail.com>
2024-02-23 22:14:01 -0300

    feat: add method in contract for auth repository

--- src/modules/auth/application/repository/auth-repository.ts
+++ src/modules/auth/application/repository/auth-repository.ts
@@ -1,6 +1,12 @@
 import { User } from '@/modules/users/domain/user';
 
+export interface UpdateForgotPasswordInput {
+  id: string;
+  token: string;
+}
+
 export interface AuthRepository {
   findByEmail(email: string): Promise<User>;
   findById(id: string): Promise<User>;
+  updateForgotPasswordToken(params: UpdateForgotPasswordInput): Promise<void>;
 }

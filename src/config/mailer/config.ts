35b5c0894ecaff958857bc092e31c506152ed0f7
Israel Batista <israelbatiista19@gmail.com>
2024-02-25 13:37:37 -0300

    feat: set config mailer

---
+++ src/config/mailer/config.ts
@@ -0,0 +1,6 @@
+export default {
+  senderDefault: 'contato@evnts.com.br',
+  devEmails: ['dev@evnts.com.br'],
+  bccEmails: ['eventosdigitais@evnts.com.br'],
+  team_project: 'projetos@evnts.com.br',
+};
